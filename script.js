3d7f74e4c81cb89d513a720e3f43ec95c020e74f
dadasokeskar <dhirajkeskar2593@gmail.com>
2022-10-06 13:02:46 +0530

    first

---
+++ script.js
@@ -0,0 +1,56 @@
+// let a= prompt("Enter the value")
+
+// //a=Number.parseInt(n)
+// let count=0;
+// while(count<a)
+// {
+//     count++;
+// document.write(a)
+
+// }
+
+// let num=prompt("Enter the number");
+
+// for(count=0;count<num;count++)
+// {
+//     document.write(count);
+// }
+let total=0;
+for(var i=0; i<5;i++)
+{
+    var j=prompt("enter number");
+    total=total+parseInt(j);
+
+}
+document.write(total)
+let total1=0;
+while(count<5)
+{
+    var count=prompt("enter number");
+    total1=total1+parseInt(count);
+    count++;
+}
+// function twoDimensionArray(a, b) {
+//     let arr = [];
+
+//     // creating two dimensional array
+//     for (let i = 0; i< a; i++) {
+//         for(let j = 0; j< b; j++) {
+//             arr[i] = [];
+//         }
+//     }
+
+//     // inserting elements to array
+//     for (let i = 0; i< a; i++) {
+//         for(let j = 0; j< b; j++) {
+//             arr[i][j] = j;
+//         }
+//     }
+//     return arr;
+// }
+
+// const x = 2;
+// const y = 3;
+
+// const result = twoDimensionArray(x, y);
+// console.log(result);
